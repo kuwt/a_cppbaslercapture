07393aef3f0b95f96796ae5090c8aac9e4383e4a
kuwingto <kuwingto@gmail.com>
2020-04-17 15:38:09 +0800

    enhance live video

--- src/test_baslerCapture.cpp
+++ src/test_baslerCapture.cpp
@@ -3,21 +3,66 @@
 
 #include "baslerCapture.h"
 #include <iostream>
-#include <Windows.h>
 #include <thread>
+#include <chrono>  // for high_resolution_clock
+
 
 int liveStreamThread(std::shared_ptr<baslerCaptureItf> pCapture)
 {
 	while (pCapture->getCurrentState() == baslerCaptureItf::RUNNING_STATE)
 	{
+		auto starttime = std::chrono::steady_clock::now();
+
+		/***** get images *****/
 		std::vector<cv::Mat> mats;
 		int status = pCapture->ExecuteSWTrig(mats);
-		if (status == 0 && !mats.empty())
+		if (status != 0 && mats.empty())
 		{
-			cv::imshow("livevideo", mats[0]);
-			cv::waitKey(1);
+			continue;
 		}
-		Sleep(50);
+
+		bool isAnyEmopty = false;
+		for (int i = 0; i < mats.size(); ++i)
+		{
+			if (mats[i].empty())
+			{
+				isAnyEmopty = true;
+			}
+		}
+		if (isAnyEmopty)
+		{
+			continue;
+		}
+
+		/***** concat *****/
+		cv::Mat mat_hconcat = mats[0];
+		for (int i = 1; i < mats.size(); ++i)
+		{
+			cv::hconcat(mat_hconcat, mats[i], mat_hconcat);
+		}
+			
+		/***** resize *****/
+		int targetHeight = 500;
+		float scaleFactor = float(targetHeight) / float(mat_hconcat.size().height);
+		int targetWidth = int(mat_hconcat.size().width * scaleFactor);
+		cv::Mat mat_hconcat_resize;
+		cv::resize(mat_hconcat, mat_hconcat_resize, cv::Size(targetWidth, targetHeight));
+
+		/***** fps *****/
+		auto endtime = std::chrono::steady_clock::now();
+		float timeElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(endtime - starttime).count();
+		float fps = 1.0 * 1000 / timeElapsed;
+
+		char buffer[1024];
+		snprintf(buffer, 1024, "fps = %f Hz", fps);
+		cv::putText(mat_hconcat_resize, buffer,
+			cvPoint(30, 30), cv::FONT_HERSHEY_COMPLEX_SMALL,
+			0.8, cvScalar(200, 200, 250), 1, CV_AA);
+
+		/***** show *****/
+		cv::imshow("livevideo", mat_hconcat_resize);
+		cv::waitKey(1);
+
 	}
 	return 0;
 }
