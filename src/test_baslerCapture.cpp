bc57dde0128c6fcfefeee95cf166bf25b6861ca7
kuwingto <kuwingto@gmail.com>
2020-04-21 19:39:15 +0800

    bugfix starting idx

--- src/test_baslerCapture.cpp
+++ src/test_baslerCapture.cpp
@@ -117,7 +117,7 @@ int main(int argc, char *argv[])
 
 	/************ service loop ***************/
 	int status = 0;
-	int counter = 0;
+	int counter = startImgIdx;
 	std::thread t(liveStreamThread, pCapture);
 	while (1)
 	{
